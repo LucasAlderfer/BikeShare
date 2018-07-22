0451fb5385d5e60a77e31c37ec404dd015ee1a51
Chris Powell <chris.ian.powell@gmail.com>
2018-07-22 14:33:25 -0600

    adds welcome mvc, sessions mvc, users mvc and dashboard mvc as well as user can create a new account functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180721200019) do
+ActiveRecord::Schema.define(version: 20180722183939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20180721200019) do
     t.string "city"
     t.date "installation_date"
     t.integer "initial_bike_count"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
