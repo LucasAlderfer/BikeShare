fc94c60314e95a54e69baeb5764a666660410666
Angela Duran <duranangela19@gmail.com>
2018-07-24 06:56:07 -0600

    Add model tests to order and order_accessory for total and subtotal

--- spec/models/order_accessory_spec.rb
+++ spec/models/order_accessory_spec.rb
@@ -10,4 +10,16 @@ describe OrderAccessory do
     it {should belong_to(:order)}
     it {should belong_to(:accessory)}
   end
+  describe 'instance methods' do
+    it 'should give the subtotal of the order_accessory' do
+      user = User.create(username: "asdf", password: "asdf")
+      acc1 = Accessory.create(title: 'bike thing', description: 'does things', price: 2, status: 'Active', image: 'pic.jpg')
+      order_1 = user.orders.create(status: 2)
+      ord_acc = order_1.order_accessories.create(quantity: 2, accessory_id: acc1.id)
+
+      expected = 4
+
+      expect(ord_acc.subtotal).to eq(expected)
+    end
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -10,4 +10,18 @@ describe Order do
     it {should have_many(:order_accessories)}
     it {should have_many(:accessories).through(:order_accessories)}
   end
+  context 'Instance Methods' do
+    it 'should give the total of all subtotals' do
+      user = User.create(username: "asdf", password: "asdf")
+      acc1 = Accessory.create(title: 'bike thing', description: 'does things', price: 2, status: 'Active', image: 'pic.jpg')
+      acc2 = Accessory.create(title: 'other thing', description: 'does others', price: 4, status: 'Active', image: 'img.jpg')
+      order_1 = user.orders.create(status: 2)
+      order_1.order_accessories.create(quantity: 2, accessory_id: acc1.id)
+      order_1.order_accessories.create(quantity: 1, accessory_id: acc2.id)
+
+      expected = 8
+
+      expect(order_1.total).to eq(expected)
+    end
+  end
 end
