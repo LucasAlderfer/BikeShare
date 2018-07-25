dc1f1f3ca9c6e3c46705baa5a04cd6dc8c71d58a
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-07-24 22:37:52 -0600

    Changes route from trips/dashboard to trips-dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   get '/dashboard', to: 'dashboard#index'
 
-  get '/trips/dashboard', to: 'trips#dashboard'
+  get '/trips-dashboard', to: 'trips#dashboard'
 
 
   get '/stations-dashboard', to: 'stations#dashboard'
