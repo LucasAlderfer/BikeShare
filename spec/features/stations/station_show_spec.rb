fc87a3882bbbc472380a7bcbf3cd8cfab3ff260d
MacInnes <califormula@gmail.com>
2018-07-24 21:00:39 -0600

    adds an actual user to the feature test...

--- spec/features/stations/station_show_spec.rb
+++ spec/features/stations/station_show_spec.rb
@@ -21,6 +21,8 @@ end
 describe 'as a user' do
   context 'visits /stations/:id' do
     it 'sees station show page' do
+      user = User.create(username: "asdf", password: "asdf")
+      
       station_1 = Station.create(name: 'hello', dock_count: 2, city:'Denver', installation_date:'8/7/2010')
       station_2 = Station.create(name: 'world', dock_count: 3, city:'Chicago', installation_date:'8/7/2010')
 
@@ -31,6 +33,8 @@ describe 'as a user' do
       trip_5 = Trip.create!(duration:8, start_date:'8/9/2010', start_station_id: 1, end_date: '8/9/2010', end_station_id: 2, bike_id: 4, subscription_type: "Customer", zip_code: 22207)
       trip_6 = Trip.create!(duration:8, start_date:'8/12/2010', start_station_id: 1, end_date: '8/12/2010', end_station_id: 2, bike_id: 3, subscription_type: "Subscriber", zip_code: 22207)
 
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
       # not completely sure this will work, might need to visit '/stations/hello'
       visit station_path(station_1.name)
 
