5d2eeb32643c27ce1d5bb1034c5df89b7156be28
Chris Powell <chris.ian.powell@gmail.com>
2018-07-25 18:02:36 -0600

    adds slug fix

--- spec/features/stations/station_show_spec.rb
+++ spec/features/stations/station_show_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 describe 'as a visitor' do
   context 'visits /station/:id' do
     it 'sees station show page' do
-      station_1 = Station.create(name: 'hello', dock_count:2, city:'Denver', installation_date:'8/7/2010')
+      station_1 = Station.create(name: 'hello dood', dock_count:2, city:'Denver', installation_date:'8/7/2010')
 
       visit stations_path
 
       click_on station_1.name
 
-      expect(current_path).to eq '/stations/hello'
+      expect(current_path).to eq "/stations/#{station_1.slug}"
       expect(page).to have_content station_1.name
       expect(page).to have_content station_1.city
       expect(page).to have_content station_1.dock_count
